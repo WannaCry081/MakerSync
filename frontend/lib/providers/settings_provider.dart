8ce360b9dda6cce22113446b610edb5d5882876c
Lirae Que Data <liraedata59@gmail.com>
2024-03-04 23:32:30 +0800

    Add _init method in settings_provider.dart

--- frontend/lib/providers/settings_provider.dart
+++ frontend/lib/providers/settings_provider.dart
@@ -1,6 +1,15 @@
 import "package:flutter/material.dart";
+import "package:shared_preferences/shared_preferences.dart";
 
 
 class SettingsProvider with ChangeNotifier {
+  SharedPreferences? _pref;
 
+  SettingsProvider() {
+    _init();
+  }
+
+  Future<void> _init() async {
+    _pref = await SharedPreferences.getInstance();
+  }
 }
