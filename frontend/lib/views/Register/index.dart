54eef78f32914dd0a0c0a9cd8720e7f9a7ab7807
Lirae Que Data <liraedata59@gmail.com>
2024-03-05 00:13:53 +0800

    Add RegisterView class in Register/index.dart

---
+++ frontend/lib/views/Register/index.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+
+class RegisterView extends StatelessWidget {
+  const RegisterView({ super.key });
+
+  @override
+  Widget build(BuildContext context){
+    return const Scaffold(
+      body : Center(
+        child : Text("Register Page")
+      )
+    );
+  }
+}
