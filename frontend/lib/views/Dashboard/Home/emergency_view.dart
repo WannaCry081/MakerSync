308e943d2a9ed11d22000d304805f0fbba31e464
shielamae02 <shiela.mlepon@gmail.com>
2024-03-05 23:52:46 +0800

    Update emergency view content

--- frontend/lib/views/Dashboard/Home/emergency_view.dart
+++ frontend/lib/views/Dashboard/Home/emergency_view.dart
@@ -1,10 +1,63 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:frontend/widgets/text_widget.dart';
 
 class EmergencyView extends StatelessWidget {
   const EmergencyView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("EmergencyView"));
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: (){},
+              child: Stack(
+                children:[
+                  Container(
+                    height: 260.h, 
+                    width: 260.w,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.grey.shade300,
+                    )
+                  ),
+                  Positioned.fill(
+                    child: Center(
+                      child: ElevatedButton(
+                       onPressed: (){},
+                        style: ElevatedButton.styleFrom(
+                          shape: const CircleBorder(),
+                          padding: EdgeInsets.symmetric(
+                            vertical: 70.h,
+                            horizontal: 70.w),
+                          backgroundColor: Colors.red[400],
+                          foregroundColor: Colors.white,
+                        ),
+                        child: MSTextWidget(
+                          "STOP",
+                          fontSize: 40.sp,
+                          fontWeight: FontWeight.bold
+                        )
+                      ),
+                    ),
+                  )
+                ]
+              ),
+            ),
+
+            SizedBox(height: 40.h),
+
+            MSTextWidget(
+              "Press the button to initiate an emergency stop of the machine.",
+              fontSize: 16.sp,
+              textAlign: TextAlign.center,
+            )
+          ],
+        )
+      )
+    );
   }
 }
