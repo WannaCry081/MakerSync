6cc749717a4b212e6568614e6beb3179bc7effe4
Lirae Que Data <liraedata59@gmail.com>
2024-03-05 18:35:28 +0800

    Add MSWrapperWidget in ForgotPassword/index.dart

--- frontend/lib/views/ForgotPassword/index.dart
+++ frontend/lib/views/ForgotPassword/index.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import "package:flutter_screenutil/flutter_screenutil.dart";
+import "package:frontend/widgets/wrapper_widget.dart";
 
 
 class ForgotPasswordView extends StatefulWidget {
@@ -11,9 +13,18 @@ class ForgotPasswordView extends StatefulWidget {
 class _ForgotPasswordViewState extends State<ForgotPasswordView> {
   @override
   Widget build(BuildContext context){
-    return const Scaffold(
-      body : Center(
-        child : Text("hello world")
+    return Scaffold(
+      body : MSWrapperWidget(
+        child : Padding(
+          padding : EdgeInsets.symmetric(
+            horizontal: 22.w,
+            vertical: 22.h
+          ),
+
+          child : Center(
+            child : Text("Hello World")
+          )
+        )
       )
     );
   }
