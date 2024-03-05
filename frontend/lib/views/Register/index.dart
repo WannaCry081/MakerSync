5e61281540f801e4a5d78f7ba90809ac06a3b573
Lirae Que Data <liraedata59@gmail.com>
2024-03-05 17:57:19 +0800

    Add wrapper widget in Register/index.dart

--- frontend/lib/views/Register/index.dart
+++ frontend/lib/views/Register/index.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
-
+import "package:flutter_screenutil/flutter_screenutil.dart";
+import "package:frontend/widgets/wrapper_widget.dart";
 
 class RegisterView extends StatefulWidget {
   const RegisterView({ super.key });
@@ -11,10 +12,19 @@ class RegisterView extends StatefulWidget {
 class _RegisterViewState extends State<RegisterView> {
   @override
   Widget build(BuildContext context){
-    return const Scaffold(
-      body : Center(
-        child : Text("Register Page")
-      )
+    return Scaffold(
+      body : MSWrapperWidget(
+        child : Padding(
+          padding : EdgeInsets.symmetric(
+            horizontal: 22.w,
+            vertical: 22.h
+          ),
+
+          child : Center(
+            child : Text("Hello world")
+          )
+        )
+      ) 
     );
   }
 }
