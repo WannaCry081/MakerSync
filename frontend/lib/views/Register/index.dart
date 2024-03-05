a912210ae1ecedc45baa7b5488d2da842495d8b8
Lirae Que Data <liraedata59@gmail.com>
2024-03-05 17:53:21 +0800

    Update RegisterView class to stateful widget

--- frontend/lib/views/Register/index.dart
+++ frontend/lib/views/Register/index.dart
@@ -1,9 +1,14 @@
 import "package:flutter/material.dart";
 
 
-class RegisterView extends StatelessWidget {
+class RegisterView extends StatefulWidget {
   const RegisterView({ super.key });
 
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
   @override
   Widget build(BuildContext context){
     return const Scaffold(
