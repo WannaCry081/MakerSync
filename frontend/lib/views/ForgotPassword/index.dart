d73a1e0ed9adec8a8602d6f9b5463496f7b662dc
Lirae Que Data <liraedata59@gmail.com>
2024-03-05 17:50:19 +0800

    Add ForgotPasswordView  class in ForgotPassword/index.dart

---
+++ frontend/lib/views/ForgotPassword/index.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+
+class ForgotPasswordView extends StatelessWidget {
+  const ForgotPasswordView({ super.key });
+
+  @override
+  Widget build(BuildContext context){
+    return const Scaffold(
+      body : Center(
+        child : Text("hello world")
+      )
+    );
+  }
+}
