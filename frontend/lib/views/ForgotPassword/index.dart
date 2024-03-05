234dcc3a7b53170b462c9391a5cbf478dd964119
Lirae Que Data <liraedata59@gmail.com>
2024-03-05 18:38:23 +0800

    Add content method in ForgotPassword/index.dart

--- frontend/lib/views/ForgotPassword/index.dart
+++ frontend/lib/views/ForgotPassword/index.dart
@@ -1,6 +1,10 @@
 import "package:flutter/material.dart";
 import "package:flutter_screenutil/flutter_screenutil.dart";
+import "package:frontend/widgets/back_button_widget.dart";
 import "package:frontend/widgets/wrapper_widget.dart";
+import "package:frontend/widgets/button_widget.dart";
+import "package:frontend/widgets/text_widget.dart";
+import "package:frontend/widgets/textfield_widget.dart";
 
 
 class ForgotPasswordView extends StatefulWidget {
@@ -11,6 +15,9 @@ class ForgotPasswordView extends StatefulWidget {
 }
 
 class _ForgotPasswordViewState extends State<ForgotPasswordView> {
+
+  final TextEditingController _email = TextEditingController(text : "");
+  
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -22,9 +29,67 @@ class _ForgotPasswordViewState extends State<ForgotPasswordView> {
           ),
 
           child : Center(
-            child : Text("Hello World")
+            child : content()
           )
         )
+      )
+    );
+  }
+
+  Widget content() {
+    return Form(
+      child : Column(
+        children : [
+          MSBackButtonWidget(
+            btnOnTap: (){},
+          ),
+
+          SizedBox(height : 30.h),
+          
+          MSTextWidget(
+            "Forgot Password",
+            fontSize: 26.sp,
+            fontColor: Theme.of(context).colorScheme.primary,
+            fontWeight: FontWeight.bold,
+          ),
+          
+          SizedBox(height : 5.h),
+                
+          const MSTextWidget(
+            "Effortless account recovery! Enter your email address to\nquickly restore access and track your progress.",
+          ),
+      
+          SizedBox(height : 30.h),
+      
+          MSTextFieldWidget(
+            controller : _email,
+            fieldLabelText: "Email Address",
+            fieldBackground: (Theme.of(context).brightness == Brightness.dark) 
+              ? Theme.of(context).colorScheme.tertiary
+              : Colors.grey.shade50,
+            fieldBorderColor: (Theme.of(context).brightness == Brightness.dark)
+              ? Colors.grey.shade600
+              : Theme.of(context).colorScheme.tertiary,
+            fieldLabelColor: (Theme.of(context).brightness == Brightness.dark) 
+              ? Colors.grey.shade400 
+              : Colors.grey.shade600
+          ),
+      
+          SizedBox(height : 30.h),
+          
+          MSButtonWidget(
+            btnOnTap: (){},
+            btnColor : Theme.of(context).colorScheme.primary,
+            child : Center(
+              child : MSTextWidget(
+                "Submit",
+                fontSize : 16.sp,
+                fontColor : Colors.white,
+                fontWeight: FontWeight.w600
+              )
+            )
+          ),
+        ]
       )
     );
   }
