ca7c184b8b9ec96a7b7f07cb4fd0cf646f35f3bb
Lirae Que Data <liraedata59@gmail.com>
2024-03-05 18:01:59 +0800

    Add content method in Register/index.dart

--- frontend/lib/views/Register/index.dart
+++ frontend/lib/views/Register/index.dart
@@ -1,5 +1,9 @@
 import "package:flutter/material.dart";
 import "package:flutter_screenutil/flutter_screenutil.dart";
+import "package:frontend/widgets/back_button_widget.dart";
+import "package:frontend/widgets/button_widget.dart";
+import "package:frontend/widgets/text_widget.dart";
+import "package:frontend/widgets/textfield_widget.dart";
 import "package:frontend/widgets/wrapper_widget.dart";
 
 class RegisterView extends StatefulWidget {
@@ -10,6 +14,12 @@ class RegisterView extends StatefulWidget {
 }
 
 class _RegisterViewState extends State<RegisterView> {
+
+  final TextEditingController _name = TextEditingController(text : "");
+  final TextEditingController _email = TextEditingController(text : "");
+  final TextEditingController _password = TextEditingController(text : "");
+  final TextEditingController _rePassword = TextEditingController(text : "");
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -21,10 +31,139 @@ class _RegisterViewState extends State<RegisterView> {
           ),
 
           child : Center(
-            child : Text("Hello world")
+            child : content()
           )
         )
       ) 
+    );
+  }
+
+  Widget content(){
+    return Form(
+      child: Column(
+        children : [
+          MSBackButtonWidget(
+            btnOnTap: (){},
+          ),
+      
+          SizedBox(height : 30.h),
+          
+          MSTextWidget(
+            "Get Started!",
+            fontSize: 26.sp,
+            fontColor: Theme.of(context).colorScheme.primary,
+            fontWeight: FontWeight.bold,
+          ),
+          
+          SizedBox(height : 5.h),
+                
+          const MSTextWidget(
+            "It's free, secure and easy. Create an account and\nstart tracking your progress in one go!",
+          ),
+      
+          SizedBox(height : 30.h),
+      
+          MSTextFieldWidget(
+            controller : _name,
+            fieldLabelText: "Full Name",
+            fieldBackground: (Theme.of(context).brightness == Brightness.dark) 
+              ? Theme.of(context).colorScheme.tertiary
+              : Colors.grey.shade50,
+            fieldBorderColor: (Theme.of(context).brightness == Brightness.dark)
+              ? Colors.grey.shade600
+              : Theme.of(context).colorScheme.tertiary,
+            fieldLabelColor: (Theme.of(context).brightness == Brightness.dark) 
+              ? Colors.grey.shade400 
+              : Colors.grey.shade600
+          ),
+      
+          SizedBox(height : 15.h),
+
+          MSTextFieldWidget(
+            controller : _email,
+            fieldLabelText: "Email Address",
+            fieldBackground: (Theme.of(context).brightness == Brightness.dark) 
+              ? Theme.of(context).colorScheme.tertiary
+              : Colors.grey.shade50,
+            fieldBorderColor: (Theme.of(context).brightness == Brightness.dark)
+              ? Colors.grey.shade600
+              : Theme.of(context).colorScheme.tertiary,
+            fieldLabelColor: (Theme.of(context).brightness == Brightness.dark) 
+              ? Colors.grey.shade400 
+              : Colors.grey.shade600
+          ),
+      
+          SizedBox(height : 15.h),
+      
+          MSTextFieldWidget(
+            controller : _password,
+            fieldIsObsecure: true,
+            fieldLabelText: "Password",
+            fieldBackground: (Theme.of(context).brightness == Brightness.dark) 
+              ? Theme.of(context).colorScheme.tertiary
+              : Colors.grey.shade50,
+            fieldBorderColor: (Theme.of(context).brightness == Brightness.dark)
+              ? Colors.grey.shade600
+              : Theme.of(context).colorScheme.tertiary,
+            fieldLabelColor: (Theme.of(context).brightness == Brightness.dark) 
+              ? Colors.grey.shade400 
+              : Colors.grey.shade600
+          ),      
+
+          SizedBox(height : 15.h),
+      
+          MSTextFieldWidget(
+            controller : _rePassword,
+            fieldIsObsecure: true,
+            fieldLabelText: "Confirm Password",
+            fieldBackground: (Theme.of(context).brightness == Brightness.dark) 
+              ? Theme.of(context).colorScheme.tertiary
+              : Colors.grey.shade50,
+            fieldBorderColor: (Theme.of(context).brightness == Brightness.dark)
+              ? Colors.grey.shade600
+              : Theme.of(context).colorScheme.tertiary,
+            fieldLabelColor: (Theme.of(context).brightness == Brightness.dark) 
+              ? Colors.grey.shade400 
+              : Colors.grey.shade600
+          ),      
+      
+          SizedBox(height : 30.h),
+          
+          MSButtonWidget(
+            btnOnTap: (){},
+            btnColor : Theme.of(context).colorScheme.primary,
+            child : Center(
+              child : MSTextWidget(
+                "Sign Up",
+                fontSize : 16.sp,
+                fontColor : Colors.white,
+                fontWeight: FontWeight.w600
+              )
+            )
+          ),
+      
+          const Spacer(),
+      
+          SizedBox(height : 10.h),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children : [
+              const MSTextWidget(
+                "Already have an account? ",
+              ),
+      
+              GestureDetector(
+                onTap : (){},
+                child : MSTextWidget(
+                  "Sign In", 
+                  fontColor: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.w600,
+                )
+              )
+            ]
+          )
+        ]
+      ),
     );
   }
 }
