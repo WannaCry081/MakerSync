ddbaad9b53692b1aec849999d999771d50cafe08
shielamae02 <shiela.mlepon@gmail.com>
2024-03-05 23:53:42 +0800

    Update members view content

--- frontend/lib/views/Dashboard/Home/members_view.dart
+++ frontend/lib/views/Dashboard/Home/members_view.dart
@@ -1,10 +1,87 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:frontend/widgets/text_widget.dart';
 
 class MembersView extends StatelessWidget {
   const MembersView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("MembersView"));
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.only(top: 20.h),
+          child: ListView.builder(
+            itemCount: 5,
+            itemBuilder: (context, index){
+              return memberCard(
+                context, "John Doe", "johndoe@gmail.com"
+              );
+            }
+          ),
+        )
+      )
+    );
+  }
+
+  Widget memberCard(context, name, email) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 7.5.h),
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          vertical: 12.h,
+          horizontal: 12.w
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8.r),
+          color: Theme.of(context).colorScheme.tertiary,
+        ),
+        child: Row(
+          children: [
+            Stack(
+              children: [
+                SvgPicture.asset(
+                  "assets/svgs/Logo.svg"
+                ),
+                Positioned(
+                  bottom: 0, right: 0,
+                  child: Container(
+                    height: 12.h,
+                    width: 12.w,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.green.shade400
+                    ),
+                  ),
+                )
+              ],
+            ),
+
+            SizedBox(width: 12.h),
+
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                MSTextWidget(
+                  "$name",
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.w500,
+                  fontColor: Theme.of(context).colorScheme.onBackground,
+                  
+                ),
+                MSTextWidget(
+                  "$email",
+                  fontWeight: FontWeight.w500,
+                  fontColor: Colors.grey.shade600,
+                  fontHeight: 1.h,
+                  
+                ),
+              ],
+            ),
+          ],
+        )
+      ),
+    );
   }
 }
