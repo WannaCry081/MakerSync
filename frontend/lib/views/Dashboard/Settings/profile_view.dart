82192fcc83f8054a6a5bf5a91dec3d8989a0d376
shielamae02 <shiela.mlepon@gmail.com>
2024-03-06 13:47:22 +0800

    Add navigate function to profile view

--- frontend/lib/views/Dashboard/Settings/profile_view.dart
+++ frontend/lib/views/Dashboard/Settings/profile_view.dart
@@ -1,10 +1,33 @@
 import "package:flutter/material.dart";
+import "package:frontend/views/Dashboard/Settings/index.dart";
+import "package:frontend/widgets/back_button_widget.dart";
 
-class ProfileView extends StatelessWidget {
+class ProfileView extends StatefulWidget {
   const ProfileView({super.key});
 
   @override
+  State<ProfileView> createState() => _ProfileViewState();
+}
+
+class _ProfileViewState extends State<ProfileView> {
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Text("ProfileView"));
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        MSBackButtonWidget(
+          btnOnTap: navigateToSettings
+        ),
+
+      ]
+    );
+  }
+
+  void navigateToSettings(){
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const SettingsView()
+      )
+    );
   }
 }
