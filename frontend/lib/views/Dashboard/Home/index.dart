d6d08319211cb9e3d380ab6b61c8eb1390fc54b9
shielamae02 <shiela.mlepon@gmail.com>
2024-03-06 13:18:57 +0800

    Add wrapper widget to home view

--- frontend/lib/views/Dashboard/Home/index.dart
+++ frontend/lib/views/Dashboard/Home/index.dart
@@ -6,6 +6,7 @@ import 'package:frontend/views/Dashboard/Home/emergency_view.dart';
 import 'package:frontend/views/Dashboard/Home/members_view.dart';
 import 'package:frontend/views/Dashboard/Home/overview_view.dart';
 import 'package:frontend/widgets/text_widget.dart';
+import 'package:frontend/widgets/wrapper_widget.dart';
 
 
 class HomeView extends StatefulWidget {
@@ -21,94 +22,98 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: 22.w,
-              vertical: 22.h),
-            child: Column(
-              children: [ 
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-
-                      SizedBox(height: 15.h),
-
-                      MSTextWidget(
-                        "Hello, Shiela!",
-                        fontSize: 26.sp,
-                        fontWeight: FontWeight.bold,
-                        fontColor: Theme.of(context).colorScheme.primary
-                      ),
-
-                      const MSTextWidget("Let's start tracking your progress!")
-                    ],
-                  ),
-
-                  SvgPicture.asset(
-                      "assets/svgs/Logo.svg",
-                      height: 48.h
-                    )
-                  ],
-                ),
-
-                SizedBox(height: 35.h),
-
-                SizedBox(
-                  height: 45.h,
-                  child: AdvancedSegment(
-                    controller: _controller,
-                    segments: const {
-                      "Overview": "Overview",
-                      "Members" : "Members",
-                      "Emergency" : "Emergency",
-                    },
-                    activeStyle: TextStyle(
-                      fontFamily: "Inter",
-                      fontSize: 16.sp,
-                      color: Colors.white
-                    ),
-                    inactiveStyle: TextStyle(
-                      fontFamily: "Inter",
-                      fontSize: 16.sp,
-                      color: Theme.of(context).colorScheme.onBackground
-                    ),
-                    sliderOffset: 6,
-                    sliderDecoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5.r),
-                      color: Theme.of(context).colorScheme.primary
-                    ),
-                    backgroundColor: Theme.of(context).colorScheme.tertiary
-                  )
-                ),
-
-                Expanded(
-                  child: ValueListenableBuilder<String>(
-                    valueListenable: _controller,
-                    builder: (_, key, __) {
-                      switch (key) {
-                        case "Overview":
-                          return const OverviewView();
-                        case "Members":
-                          return const MembersView();
-                        case "Emergency":
-                           return const EmergencyView();
-                        default:
-                          return const OverviewView();
-                      }
-                    },
-                  ),
-                ),
-              ],
-            ),
+      body: MSWrapperWidget(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 22.w,
+            vertical: 22.h
           ),
+
+          child: content()
         ),
       )
+    );
+  }
+
+  Widget content(){
+    return Column(
+      children: [ 
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+
+              SizedBox(height: 15.h),
+
+              MSTextWidget(
+                "Hello, Shiela!",
+                fontSize: 26.sp,
+                fontWeight: FontWeight.bold,
+                fontColor: Theme.of(context).colorScheme.primary
+              ),
+
+              const MSTextWidget("Let's start tracking your progress!")
+            ],
+          ),
+
+          SvgPicture.asset(
+              "assets/svgs/Logo.svg",
+              height: 48.h
+            )
+          ],
+        ),
+
+        SizedBox(height: 35.h),
+
+        SizedBox(
+          height: 45.h,
+          child: AdvancedSegment(
+            controller: _controller,
+            segments: const {
+              "Overview": "Overview",
+              "Members" : "Members",
+              "Emergency" : "Emergency",
+            },
+            activeStyle: TextStyle(
+              fontFamily: "Inter",
+              fontSize: 16.sp,
+              color: Colors.white
+            ),
+            inactiveStyle: TextStyle(
+              fontFamily: "Inter",
+              fontSize: 16.sp,
+              color: Theme.of(context).colorScheme.onBackground
+            ),
+            sliderOffset: 6,
+            sliderDecoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5.r),
+              color: Theme.of(context).colorScheme.primary
+            ),
+            backgroundColor: Theme.of(context).colorScheme.tertiary
+          )
+        ),
+
+        Expanded(
+          child: ValueListenableBuilder<String>(
+            valueListenable: _controller,
+            builder: (_, key, __) {
+              switch (key) {
+                case "Overview":
+                  return const OverviewView();
+                case "Members":
+                  return const MembersView();
+                case "Emergency":
+                    return const EmergencyView();
+                default:
+                  return const OverviewView();
+              }
+            },
+          ),
+        ),
+      ],
     );
   }
 } 
