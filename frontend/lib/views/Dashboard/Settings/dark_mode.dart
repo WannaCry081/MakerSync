541a345899edb5032adf9e672e8a1f3ac738c44c
shielamae02 <shiela.mlepon@gmail.com>
2024-03-06 12:55:44 +0800

    Add dark mode view

---
+++ frontend/lib/views/Dashboard/Settings/dark_mode.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+
+class DarkModeView extends StatelessWidget {
+  const DarkModeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("DarkModeView"));
+  }
+}
