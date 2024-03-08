b29d0fd9569b34254f2f4eedffe7c3ea5294e4b6
shielamae02 <shiela.mlepon@gmail.com>
2024-03-08 15:25:04 +0800

    Add snackbar custom widget

---
+++ frontend/lib/widgets/snackbar_widget.dart
@@ -0,0 +1,37 @@
+import "package:flutter/material.dart";
+import "package:flutter_screenutil/flutter_screenutil.dart";
+import "package:frontend/widgets/text_widget.dart";
+
+
+class MSSnackbarWidget extends StatelessWidget {
+  final String message;
+
+  const MSSnackbarWidget({
+    super.key,
+    required this.message
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+   void showSnackbar(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: MSTextWidget(message),
+        duration: const Duration(milliseconds: 1500),
+        padding: EdgeInsets.symmetric(horizontal: 10.w),
+        behavior: SnackBarBehavior.floating,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.r)),
+        action: SnackBarAction(
+          label: "Close",
+          onPressed: () {
+            ScaffoldMessenger.of(context).hideCurrentSnackBar();
+          },
+        ),
+      )
+    );
+  }
+}
