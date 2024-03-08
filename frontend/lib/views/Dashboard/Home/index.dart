70b769e81838a7e59d60f9537975a39d971f224c
shielamae02 <shiela.mlepon@gmail.com>
2024-03-08 15:22:40 +0800

    Remove advanced_segment as tab controller

--- frontend/lib/views/Dashboard/Home/index.dart
+++ frontend/lib/views/Dashboard/Home/index.dart
@@ -1,27 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:flutter_advanced_segment/flutter_advanced_segment.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:frontend/views/Dashboard/Home/emergency_view.dart';
 import 'package:frontend/views/Dashboard/Home/members_view.dart';
 import 'package:frontend/views/Dashboard/Home/overview_view.dart';
 import 'package:frontend/widgets/text_widget.dart';
 import 'package:frontend/widgets/wrapper_widget.dart';
 
-
 class HomeView extends StatefulWidget {
-  const HomeView({super.key});
+  const HomeView({Key? key}) : super(key: key);
 
   @override
   State<HomeView> createState() => _HomeViewState();
 }
 
 class _HomeViewState extends State<HomeView> {
-  final _controller = ValueNotifier('Overview');
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+     return Scaffold(
       body: MSWrapperWidget(
         child: Padding(
           padding: EdgeInsets.symmetric(
@@ -35,89 +31,91 @@ class _HomeViewState extends State<HomeView> {
     );
   }
 
-  Widget content(){
-    return Column(
-      children: [ 
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-
-              SizedBox(height: 15.h),
-
-              MSTextWidget(
-                "Hello, Shiela!",
-                fontSize: 26.sp,
-                fontWeight: FontWeight.bold,
-                fontColor: Theme.of(context).colorScheme.primary
-              ),
-
-              SizedBox(height : 3.h),
-
-              const MSTextWidget(
-                "Let's start tracking your progress!")
+  Widget content() {
+    return DefaultTabController(
+      length: 3,
+      child: Column(
+        children: [
+           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+      
+                SizedBox(height: 15.h),
+      
+                MSTextWidget(
+                  "Hello, Shiela!",
+                  fontSize: 26.sp,
+                  fontWeight: FontWeight.bold,
+                  fontColor: Theme.of(context).colorScheme.primary
+                ),
+      
+                SizedBox(height : 3.h),
+      
+                const MSTextWidget(
+                  "Let's start tracking your progress!")
+              ],
+            ),
+      
+            SvgPicture.asset(
+                Theme.of(context).brightness == Brightness.dark
+                ? "assets/svgs/Logo_DarkMode.svg"
+                : "assets/svgs/Logo_LightMode.svg",
+                height: 48.h
+              )
             ],
           ),
+      
+          SizedBox(height: 30.h),
 
-          SvgPicture.asset(
-              Theme.of(context).brightness == Brightness.dark
-              ? "assets/svgs/Logo_DarkMode.svg"
-              : "assets/svgs/Logo_LightMode.svg",
-              height: 48.h
-            )
-          ],
-        ),
-
-        SizedBox(height: 35.h),
-
-        SizedBox(
-          height: 45.h,
-          width: MediaQuery.of(context).size.width,
-          child: AdvancedSegment(
-            controller: _controller,
-            segments: const {
-              "Overview": "Overview",
-              "Members" : "Members",
-              "Emergency" : "Emergency",
-            },
-            activeStyle: const TextStyle(
-              fontFamily: "Inter",
-              color: Colors.white
+          Container(
+            height: 55,
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.tertiary,
+              borderRadius: BorderRadius.circular(10.r),
             ),
-            inactiveStyle: TextStyle(
-              fontFamily: "Inter",
-              color: Theme.of(context).colorScheme.onBackground
-            ),
-            sliderOffset: 6,
-            sliderDecoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(5.r),
-              color: Theme.of(context).colorScheme.primary
-            ),
-            backgroundColor: Theme.of(context).colorScheme.tertiary
-          )
-        ),
+            width: MediaQuery.of(context).size.width,
+            child: TabBar(
+              tabs: const [
+                Tab(text: "Overview"),
+                Tab(text: "Members"),
+                Tab(text: "Emergency"),
+              ],
+              indicator: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+                borderRadius: BorderRadius.circular(5.r),
+              ),
+              indicatorSize: TabBarIndicatorSize.tab, 
+              indicatorPadding: EdgeInsets.symmetric(
+                horizontal: 6.w,
+                vertical: 6.h
+              ),
+              labelColor: Colors.white,
+              labelStyle: const TextStyle(
+                fontFamily: "Inter",
+              ),
+              unselectedLabelColor: Theme.of(context).colorScheme.onBackground,
+              splashFactory: NoSplash.splashFactory,
 
-        Expanded(
-          child: ValueListenableBuilder<String>(
-            valueListenable: _controller,
-            builder: (_, key, __) {
-              switch (key) {
-                case "Overview":
-                  return const OverviewView();
-                case "Members":
-                  return const MembersView();
-                case "Emergency":
-                    return const EmergencyView();
-                default:
-                  return const OverviewView();
-              }
-            },
+
+            ),
           ),
-        ),
-      ],
+          
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.6,
+            child:  const TabBarView(
+              children: [
+                Center(child: OverviewView()),
+                Center(child: MembersView()),
+                Center(child: EmergencyView()),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
-} 
+}
