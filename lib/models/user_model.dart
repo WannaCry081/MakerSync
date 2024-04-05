47f9a2529c242680b99ed25912c0d66f4debf246
shielamae02 <shiela.mlepon@gmail.com>
2024-04-05 18:08:44 +0800

    Update UserModel

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -11,17 +11,17 @@ class UserModel {
 
   factory UserModel.fromJson(Map <String, dynamic> json){
     return UserModel(
-      email: json['email'] as String? ?? "",
-      name: json['name'] as String? ?? "",
-      isConnected: json['isConnected'] as bool? ?? false 
+      email: json["email"] as String? ?? "",
+      name: json["name"] as String? ?? "",
+      isConnected: json["isConnected"] as bool? ?? false 
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'email' : email,
-      'name' : name,
-      'isConnected' : isConnected
+      "email" : email,
+      "name" : name,
+      "isConnected" : isConnected
     };
   }
 }
