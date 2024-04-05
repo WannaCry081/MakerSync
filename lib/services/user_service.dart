9283ec65f187ce1751f6942a00669cbfd08a72a7
shielamae02 <shiela.mlepon@gmail.com>
2024-04-05 18:29:44 +0800

    Add fetchUsers method in UserService

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,8 +1,27 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:frontend/models/user_model.dart';
+import 'package:frontend/services/api_constants.dart';
 
 
 class UserService {
-  
+
+  Future<UserModel> fetchUsers() async {
+    
+    final response = await http.get(Uri.parse("$USER_URL/$MACHINE_CODE"));
+
+    if (response.statusCode == 200) {
+        return UserModel.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+      } else if (response.statusCode == 400) {
+        throw Exception("Invalid sensor request.");
+      } else if (response.statusCode == 404) {
+        throw Exception("Sensor not found.");
+      } else if (response.statusCode == 500) {
+        throw Exception("Internal Server Error.");
+      } else {
+        throw Exception("Failed to fetch sensor.");
+      }
+
+  }
 }
