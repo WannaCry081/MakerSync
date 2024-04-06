76357f6012c870f1ec3b8c72ced9386d86309bf4
shielamae02 <shiela.mlepon@gmail.com>
2024-04-07 00:04:10 +0800

    Remove field in UserModel

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -2,21 +2,18 @@ class UserModel {
   final String email;
   final String name;
   final bool isConnected;
-  final bool isActive;
 
   const UserModel({
     required this.email,
     required this.name,
     required this.isConnected,
-    required this.isActive,
   });
 
   factory UserModel.fromJson(Map <String, dynamic> json){
     return UserModel(
       email: json["email"] as String? ?? "",
       name: json["name"] as String? ?? "",
-      isConnected: json["is_connected"] as bool? ?? false,
-      isActive: json["is_active"] as bool? ?? false,
+      isConnected: json["is_connected"] as bool? ?? false
     );
   }
 
@@ -24,8 +21,7 @@ class UserModel {
     return {
       "email" : email,
       "name" : name,
-      "is_connected" : isConnected,
-      "is_active" : isActive,
+      "is_connected" : isConnected
     };
   }
 }
