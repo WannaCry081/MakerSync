c2a32cf486bdd2295c861991ab366ed17d2a985d
shielamae02 <shiela.mlepon@gmail.com>
2024-04-09 23:31:17 +0800

    Update updateSensor method call in EmergencyView

--- lib/views/Dashboard/Home/emergency_view.dart
+++ lib/views/Dashboard/Home/emergency_view.dart
@@ -4,6 +4,7 @@ import 'package:frontend/models/sensor_model.dart';
 import 'package:frontend/providers/settings_provider.dart';
 import 'package:frontend/services/sensor_service.dart';
 import 'package:frontend/widgets/disconnected_view.dart';
+import 'package:frontend/widgets/snackbar_widget.dart';
 import 'package:frontend/widgets/text_widget.dart';
 import 'package:provider/provider.dart';
 
@@ -16,8 +17,15 @@ class EmergencyView extends StatefulWidget {
 
 class _EmergencyViewState extends State<EmergencyView> {
 
-  late Future<SensorModel> sensor;
+  late Future<List<dynamic>> sensor;
+  
   final SensorService _sensorService = SensorService();
+
+  @override
+  void initState() {
+    super.initState();
+    sensor = _sensorService.fetchSensors();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,51 +43,39 @@ class _EmergencyViewState extends State<EmergencyView> {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        GestureDetector(
-          onTap: () async {
-            await _sensorService.updateSensor(
-              counter: 0,
-              timer: 0,
-              temperature: 0,
-              isInitialized: false,
-              isStart: false,
-              isStop: true
-            );
-          },
-          child: Stack(
-            children:[
-              Container(
-                height: 260.h, 
-                width: 260.w,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Theme.of(context).brightness == Brightness.dark  
-                    ? Colors.grey.shade600
-                    : Colors.grey.shade200
-                )
-              ),
-              Positioned.fill(
-                child: Center(
-                  child: ElevatedButton(
-                    onPressed: (){},
-                    style: ElevatedButton.styleFrom(
-                      shape: const CircleBorder(),
-                      padding: EdgeInsets.symmetric(
-                        vertical: 60.h,
-                        horizontal: 60.w),
-                      backgroundColor: Colors.red[400],
-                      foregroundColor: Colors.white,
-                    ),
-                    child: MSTextWidget(
-                      "STOP",
-                      fontSize: 40.sp,
-                      fontWeight: FontWeight.bold
-                    )
-                  ),
-                ),
+        Stack(
+          children:[
+            Container(
+              height: 260.h, 
+              width: 260.w,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Theme.of(context).brightness == Brightness.dark  
+                  ? Colors.grey.shade600
+                  : Colors.grey.shade200
               )
-            ]
-          ),
+            ),
+            Positioned.fill(
+              child: Center(
+                child: ElevatedButton(
+                  onPressed: () => stopSensor(context),
+                  style: ElevatedButton.styleFrom(
+                    shape: const CircleBorder(),
+                    padding: EdgeInsets.symmetric(
+                      vertical: 60.h,
+                      horizontal: 60.w),
+                    backgroundColor: Colors.red[400],
+                    foregroundColor: Colors.white,
+                  ),
+                  child: MSTextWidget(
+                    "STOP",
+                    fontSize: 40.sp,
+                    fontWeight: FontWeight.bold
+                  )
+                ),
+              ),
+            )
+          ]
         ),
 
         SizedBox(height: 40.h),
@@ -91,5 +87,26 @@ class _EmergencyViewState extends State<EmergencyView> {
         )
       ],
     );
+  }
+
+
+  void stopSensor(context) async {
+    try {
+      await _sensorService.updateSensor(
+        counter: 0,
+        timer: 0,
+        temperature: 0,
+        isInitialized: false,
+        isStart: false,
+        isStop: true
+      );
+
+      const MSSnackbarWidget(
+        message: "You have stopped the machine operation.",
+      ).showSnackbar(context);
+
+    } catch (e) {
+      print("Error: $e");
+    }
   }
 }
