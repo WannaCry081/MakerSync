10483e47c9e8d2c50aff94e0bb3923bf14faf310
shielamae02 <shiela.mlepon@gmail.com>
2024-04-14 01:47:38 +0800

    Update ErrorView content

--- lib/views/Error/index.dart
+++ lib/views/Error/index.dart
@@ -1,4 +1,8 @@
 import "package:flutter/material.dart";
+import "package:flutter_screenutil/flutter_screenutil.dart";
+import "package:flutter_svg/svg.dart";
+import "package:frontend/widgets/text_widget.dart";
+import "package:frontend/widgets/wrapper_widget.dart";
 
 
 class ErrorView extends StatelessWidget {
@@ -6,6 +10,53 @@ class ErrorView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: MSWrapperWidget(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 22.w,
+            vertical: 22.h,
+          ),
+          
+          child: content(context),
+        ),
+      )
+    );
+  }
+
+  Widget content(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            SvgPicture.asset(
+              "assets/svgs/ErrorView.svg"
+            ),
+
+            SizedBox(height: 5.h),
+
+            MSTextWidget(
+              "Ooops!",
+              textAlign: TextAlign.center,
+              fontSize: 16.sp,
+              fontWeight: FontWeight.bold,
+              fontColor: Theme.of(context).colorScheme.onBackground,
+            ),
+
+            SizedBox(height: 4.h),
+
+            MSTextWidget(
+              "Something went wrong.",
+              textAlign: TextAlign.center,
+              fontWeight: FontWeight.w500,
+              fontColor: Colors.grey.shade600,
+              fontHeight: 2.h,
+            ),
+
+
+          ],
+        ),
+      )
+    );
   }
 }
