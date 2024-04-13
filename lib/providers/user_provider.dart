f788e0cf1bd798eed2e3983b4ec5e6f0aadd81d5
shielamae02 <shiela.mlepon@gmail.com>
2024-04-13 12:42:38 +0800

    Initialize UserProvider

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,5 +1,14 @@
 import "package:flutter/material.dart";
+import "package:frontend/models/user_model.dart";
+import "package:frontend/services/user_service.dart";
 
 class UserProvider with ChangeNotifier {
+  final UserService _userService = UserService();
+
+  UserModel? _user;
+
+  UserProvider() {
+    _user = null;
+  }
   
 }
