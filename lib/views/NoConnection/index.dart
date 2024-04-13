d729f4f386a4e1841fa511b5024ee0fd4fd33018
shielamae02 <shiela.mlepon@gmail.com>
2024-04-14 01:04:22 +0800

    Update NoConnectionView

--- lib/views/NoConnection/index.dart
+++ lib/views/NoConnection/index.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import "package:flutter_screenutil/flutter_screenutil.dart";
 import "package:flutter_svg/svg.dart";
+import "package:frontend/widgets/text_widget.dart";
 import "package:frontend/widgets/wrapper_widget.dart";
 
 class NoConnectionView extends StatelessWidget {
@@ -16,22 +17,44 @@ class NoConnectionView extends StatelessWidget {
             vertical: 22.h,
           ),
           
-          child: content(),
+          child: content(context),
         ),
       )
     );
   }
 
-  Widget content() {
+  Widget content(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Center(
-            child: SvgPicture.asset(
+      body: Center(
+        child: Column(
+          children: [
+            SvgPicture.asset(
               "assets/svgs/NoConnection.svg"
-            )
-          )
-        ],
+            ),
+
+            SizedBox(height: 5.h),
+
+            MSTextWidget(
+              "Ooops!",
+              textAlign: TextAlign.center,
+              fontSize: 16.sp,
+              fontWeight: FontWeight.bold,
+              fontColor: Theme.of(context).colorScheme.onBackground,
+            ),
+
+            SizedBox(height: 4.h),
+
+            MSTextWidget(
+              "No internet connection found.\nPlease check your internet connection and try again.",
+              textAlign: TextAlign.center,
+              fontWeight: FontWeight.w500,
+              fontColor: Colors.grey.shade600,
+              fontHeight: 2.h,
+            ),
+
+
+          ],
+        ),
       )
     );
   }
