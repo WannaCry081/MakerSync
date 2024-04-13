2bbbbdb3f5e6d36c906020ef629a446042b913d8
shielamae02 <shiela.mlepon@gmail.com>
2024-04-13 15:45:50 +0800

    Update createUser method in UserService

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -8,11 +8,23 @@ import 'package:frontend/services/api_constants.dart';
 class UserService {
 
   Future<UserModel> createUser({
-    required String email,
-    required String name
+    required String name,
+    required String email
   }) async {
+
+    final request = UserModel(
+      name: name,
+      email: email,
+      isConnected: true
+    );
     
-    final response = await http.post(Uri.parse("$USER_URL/$MACHINE_CODE"));
+    final response = await http.post(
+      Uri.parse("$USER_URL/$MACHINE_CODE"),
+      body: jsonEncode(request.toJson()),
+      headers: <String, String> {
+        "Content-Type": "application/json; charset=UTF-8"
+      }
+    );
 
     if (response.statusCode == 200) {
         return UserModel.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
@@ -21,7 +33,7 @@ class UserService {
       } else if (response.statusCode == 500) {
         throw Exception("Internal Server Error.");
       } else {
-        throw Exception("Failed to fetch users.");
+        throw Exception("Failed to create user: ${response.statusCode}");
       }
   }
 
