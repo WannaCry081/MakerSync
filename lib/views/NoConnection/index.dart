2d29e6e03f530d43cf97c043e47f884c7771e859
shielamae02 <shiela.mlepon@gmail.com>
2024-04-14 00:03:19 +0800

    Add NoConnectionView page

---
+++ lib/views/NoConnection/index.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+
+class NoConnectionView extends StatelessWidget {
+  const NoConnectionView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
