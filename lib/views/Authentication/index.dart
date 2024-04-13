3022021a73126b3d88f54216650199d09f9c2b58
shielamae02 <shiela.mlepon@gmail.com>
2024-04-14 01:49:48 +0800

    Add AuthViewBuilder page

---
+++ lib/views/Authentication/index.dart
@@ -0,0 +1,15 @@
+import "package:firebase_auth/firebase_auth.dart";
+import "package:flutter/material.dart";
+import "package:frontend/views/Dashboard/index.dart";
+import "package:frontend/views/Error/index.dart";
+import "package:frontend/views/Loading/index.dart";
+import "package:frontend/views/Onboarding/index.dart";
+
+class AuthViewBuilder extends StatelessWidget {
+  const AuthViewBuilder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
