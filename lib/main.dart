9b8aca681b9308882ac9919a1b5d8a66235fd786
shielamae02 <shiela.mlepon@gmail.com>
2024-04-13 12:34:29 +0800

    Update main function in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,18 @@ import "package:flutter_screenutil/flutter_screenutil.dart";
 
 import "package:frontend/views/Onboarding/index.dart";
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main(){
-  runApp(
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+
+   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(
