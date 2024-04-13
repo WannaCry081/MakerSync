0364616ad5caa60c6e2fd63137e11108384bce3d
shielamae02 <shiela.mlepon@gmail.com>
2024-04-14 00:14:44 +0800

    Update NoConnectionView

--- lib/views/NoConnection/index.dart
+++ lib/views/NoConnection/index.dart
@@ -1,10 +1,38 @@
 import "package:flutter/material.dart";
+import "package:flutter_screenutil/flutter_screenutil.dart";
+import "package:flutter_svg/svg.dart";
+import "package:frontend/widgets/wrapper_widget.dart";
 
 class NoConnectionView extends StatelessWidget {
   const NoConnectionView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: MSWrapperWidget(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 22.w,
+            vertical: 22.h,
+          ),
+          
+          child: content(),
+        ),
+      )
+    );
+  }
+
+  Widget content() {
+    return Scaffold(
+      body: Column(
+        children: [
+          Center(
+            child: SvgPicture.asset(
+              "assets/svgs/NoConnection.svg"
+            )
+          )
+        ],
+      )
+    );
   }
 }
