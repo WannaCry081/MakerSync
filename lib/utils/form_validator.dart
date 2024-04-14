d16f312099ba7c415fdee00fe025cf96581a3954
shielamae02 <shiela.mlepon@gmail.com>
2024-04-14 23:03:30 +0800

    Add validateEmail method in FormValidator

--- lib/utils/form_validator.dart
+++ lib/utils/form_validator.dart
@@ -1,3 +1,19 @@
 class FormValidator {
-  
+
+  String? validateEmail(String? value) {
+    RegExp expression = RegExp(
+        r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$",
+        caseSensitive: false);
+
+    if (value == null || value.isEmpty) {
+      return "Email Address is Required";
+    }
+
+    if (!expression.hasMatch(value)) {
+      return "Invalid Email Address. Please try again.";
+    }
+
+    return null;
+  }
+
 }
