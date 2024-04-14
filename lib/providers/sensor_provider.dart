e92a55197cc6d659c503f37bd36530dc52aac0fc
shielamae02 <shiela.mlepon@gmail.com>
2024-04-14 12:42:33 +0800

    Add setSensorData method in SensorProvider

--- lib/providers/sensor_provider.dart
+++ lib/providers/sensor_provider.dart
@@ -3,6 +3,20 @@ import "package:frontend/models/sensor_model.dart";
 import "package:frontend/services/sensor_service.dart";
 
 class SensorProvider with ChangeNotifier {
+  final SensorService _sensorService = SensorService();
+
+  SensorModel? _sensor;
+
+  SensorProvider() {
+    _sensor = null;
+  
+    notifyListeners();
+    return;
+  }
+
+  void setSensorData(SensorModel data) {
+    _sensor = data;
+  }
 
 
 }
