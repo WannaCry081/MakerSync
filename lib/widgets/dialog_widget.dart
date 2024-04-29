250de1f442049f43bb2002134a5d8f8698768c23
shielamae02 <shiela.mlepon@gmail.com>
2024-04-29 23:01:37 +0800

    Create DialogWidget widget

---
+++ lib/widgets/dialog_widget.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+
+class MSDialogWidget extends StatelessWidget {
+  const MSDialogWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
