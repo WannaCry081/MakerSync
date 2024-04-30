649c780bdbe62b67ca590aeb7bec57de7a426066
shielamae02 <shiela.mlepon@gmail.com>
2024-05-01 02:07:26 +0800

    Update LoginView

--- lib/views/Login/index.dart
+++ lib/views/Login/index.dart
@@ -1,5 +1,4 @@
 import "package:firebase_auth/firebase_auth.dart";
-import "package:firebase_core/firebase_core.dart";
 import "package:flutter/material.dart";
 import "package:flutter_screenutil/flutter_screenutil.dart";
 import "package:frontend/services/authentication_service.dart";
@@ -241,7 +240,7 @@ class _LoginViewState extends State<LoginView> {
               "System Error",
       ).showSnackbar(context);
 
-       setState(() => _isLoading = false);
+      setState(() => _isLoading = false);
     }
   }
 }
