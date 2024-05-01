a4eec3d1047f658a2ad873fca9538a9737ef3fda
shielamae02 <shiela.mlepon@gmail.com>
2024-05-01 15:17:08 +0800

    Add _notificationDetails method in NotificationService

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -5,6 +5,18 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 class NotificationService {
   static final _notifications = FlutterLocalNotificationsPlugin();
 
+  static Future _notificationDetails() async {
+    return const NotificationDetails(
+      android: AndroidNotificationDetails(
+        'channel id',
+        'channel name', 
+        channelDescription: 'channel description',
+        importance: Importance.max
+        
+      ),
+    );
+  }
+
   static Future showNotification({
     int id = 0,
     String? title, 
@@ -15,6 +27,6 @@ class NotificationService {
       id, 
       title, 
       body, 
-      await _notificationDetaisl(),
+      await _notificationDetails(),
   );
 }
