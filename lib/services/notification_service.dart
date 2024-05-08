e61b3479105c82d9890134a8878a1d08112a3cce
shielamae02 <shiela.mlepon@gmail.com>
2024-05-08 22:42:22 +0800

    Update createNotification method in NotificationService

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,5 +1,8 @@
 
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:frontend/models/notification_model.dart';
+import 'package:frontend/services/api_constants.dart';
 
 class NotificationService {
   
@@ -8,5 +11,27 @@ class NotificationService {
     required String content
   }) async {
 
+    final request = NotificationModel(
+      title: title,
+      content: content
+    );
+
+    final response = await http.post(
+      Uri.parse("$NOTIFICATION_URL/$MACHINE_CODE"),
+      body: jsonEncode(request.toJson()),
+      headers: <String, String> {
+        "Content-Type": "application/json; charset=UTF-8"
+      }
+    );
+
+    if (response.statusCode == 201) {
+      return NotificationModel.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+    } else if (response.statusCode == 400) {
+      throw Exception("User already exists.");
+    } else if (response.statusCode == 500) {
+      throw Exception("Internal Server Error.");
+    } else {
+      throw Exception("Failed to create user.");
+    }
   }
 }
