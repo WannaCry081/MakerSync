6829b74ed19b636d16133d7001b8eb90a63cb5c6
shielamae02 <shiela.mlepon@gmail.com>
2024-05-08 22:33:37 +0800

    Add createNotification method in NotificationService

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,4 +1,12 @@
 
+import 'package:frontend/models/notification_model.dart';
+
 class NotificationService {
   
+  Future<NotificationModel> createNotification({
+    required String title,
+    required String content
+  }) async {
+
+  }
 }
