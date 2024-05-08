84892c5705d73e48fa178f73e4538f230170f78f
shielamae02 <shiela.mlepon@gmail.com>
2024-05-08 22:03:28 +0800

    Add id property in NotificationModel

--- lib/models/notification_model.dart
+++ lib/models/notification_model.dart
@@ -1,14 +1,17 @@
 class NotificationModel {
+  final String? id;
   final String title;
   final String content;
 
   const NotificationModel({
+    this.id,
     required this.title,
     required this.content,
   });
 
   factory NotificationModel.fromJson(Map<String, dynamic> json) {
     return NotificationModel(
+      id: json["id"] as String? ?? "",
       title: json["title"] as String? ?? "",
       content: json["content"] as String? ?? ""
     );
