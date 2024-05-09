b0af68f520187c17dda9d756d4ca30d0af4b84fc
shielamae02 <shiela.mlepon@gmail.com>
2024-05-09 22:22:59 +0800

    Update sensor_url in api_constants.dart

--- lib/services/api_constants.dart
+++ lib/services/api_constants.dart
@@ -4,6 +4,6 @@ const String MACHINE_CODE = "";
 const String BASE_URL = "https://maker-sync-django.vercel.app/api/v2/machines";
 
 const String USER_URL = "$BASE_URL/$MACHINE_CODE/users";
-const String SENSOR_URL = "$BASE_URL/sensors";
+const String SENSOR_URL = "$BASE_URL/$MACHINE_CODE/sensors";
 const String NOTIFICATION_URL = "$BASE_URL/notification";
 
