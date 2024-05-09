6a59470e2045b899f74e1fc3b031d595d9b48464
shielamae02 <shiela.mlepon@gmail.com>
2024-05-09 22:41:22 +0800

    Update fetchSensor method in SensorService

--- lib/services/sensor_service.dart
+++ lib/services/sensor_service.dart
@@ -40,24 +40,24 @@ class SensorService {
     required SettingsProvider settings
   }) async {
 
-    final response = await http.get(Uri.parse("$SENSOR_URL/$MACHINE_CODE"));
+    final response = await http.get(Uri.parse(SENSOR_URL));
     
     if (response.statusCode == 200) {
 
-        final sensor = SensorModel.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
-        settings.setBool("isInitialize", sensor.isInitialized);
+    final sensor = SensorModel.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+    settings.setBool("isInitialize", sensor.isInitialized);
 
-        return SensorModel.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+    return SensorModel.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
 
-      } else if (response.statusCode == 400) {
-        throw Exception("Invalid sensor request.");
-      } else if (response.statusCode == 404) {
-        throw Exception("Sensor not found.");
-      } else if (response.statusCode == 500) { 
-        throw Exception("Internal Server Error.");
-      } else {
-        throw Exception("Failed to fetch sensor.S");
-      }
+    } else if (response.statusCode == 400) {
+      throw Exception("Invalid sensor request.");
+    } else if (response.statusCode == 404) {
+      throw Exception("Sensor not found.");
+    } else if (response.statusCode == 500) { 
+      throw Exception("Internal Server Error.");
+    } else {
+      throw Exception("Failed to fetch sensor.");
+    }
   }
 
   Future<SensorModel> updateSensor({
