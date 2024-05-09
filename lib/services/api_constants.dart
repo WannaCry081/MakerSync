2415cc8b50777468f67108aec2bfa3d1f8c7c50f
shielamae02 <shiela.mlepon@gmail.com>
2024-05-09 22:57:55 +0800

    Remove const in api_constants.dart

--- lib/services/api_constants.dart
+++ lib/services/api_constants.dart
@@ -1,11 +1,11 @@
-const String MACHINE_CODE = "";
+String MACHINE_CODE = "";
 
 
 const String BASE_URL = "https://maker-sync-django.vercel.app/api/v2/machines";
 
-const String USER_URL = "$BASE_URL/$MACHINE_CODE/users";
-const String SENSOR_URL = "$BASE_URL/$MACHINE_CODE/sensors";
-const String NOTIFICATION_URL = "$BASE_URL/$MACHINE_CODE/notifications";
+String USER_URL = "$BASE_URL/$MACHINE_CODE/users";
+String SENSOR_URL = "$BASE_URL/$MACHINE_CODE/sensors";
+String NOTIFICATION_URL = "$BASE_URL/$MACHINE_CODE/notifications";
 
 
 void updateMachineCode(String code) {
