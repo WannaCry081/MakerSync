e1e5b9857ed89a83735f9bdbf5519b7e687c8bf9
shielamae02 <shiela.mlepon@gmail.com>
2024-05-09 23:52:34 +0800

    Add setNotificationData method in NotificationProvider

--- lib/providers/notification_provider.dart
+++ lib/providers/notification_provider.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/models/notification_model.dart';
+import 'package:frontend/services/notification_service.dart';
 
 class NotificationProvider with ChangeNotifier{
+  final NotificationService _notificationService = NotificationService();
+ 
+  NotificationModel? _notification;
+
+  NotificationProvider() {
+    _notification = null;
+  }
+
+  void setNotificationData(NotificationModel data) {
+    _notification = data;
+  }
 
 }
