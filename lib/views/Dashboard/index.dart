0d4c2d557d5b4a4fa6bbcda1a16fd2573ddd562b
shielamae02 <shiela.mlepon@gmail.com>
2024-05-10 19:06:14 +0800

    Add NotificationCompleter in Dashboard

--- lib/views/Dashboard/index.dart
+++ lib/views/Dashboard/index.dart
@@ -1,10 +1,16 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+import 'package:frontend/providers/notification_provider.dart';
+import 'package:frontend/providers/settings_provider.dart';
 import 'package:frontend/services/local_notification_service.dart';
 import 'package:frontend/views/Dashboard/Home/index.dart';
 import 'package:frontend/views/Dashboard/Notifications/index.dart';
 import 'package:frontend/views/Dashboard/Settings/index.dart';
 import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
+import 'package:provider/provider.dart';
 
 class DashboardView extends StatefulWidget {
   DashboardView({Key? key}) : super(key:key);
@@ -15,6 +21,8 @@ class DashboardView extends StatefulWidget {
 
 class _DashboardViewState extends State<DashboardView> {
   final PersistentTabController _controller = PersistentTabController(initialIndex: 0);
+
+  Completer<void> _notificationCompleter = Completer<void>();
 
   @override 
   void initState() {
@@ -34,6 +42,16 @@ class _DashboardViewState extends State<DashboardView> {
 
     if (response != null) {
       String? payload = response;
+      String title = "";
+      String body = "";
+
+      Map<String, dynamic> data = json.decode(payload);
+      title = data['title'] ?? "";
+      body = data['body'] ?? "";
+
+      print("Payload: $payload" );
+      print("Title: $title" );
+      print("Body: $body" );
 
       print("Payload: $payload" );
       Navigator.of(context).push(
@@ -46,6 +64,26 @@ class _DashboardViewState extends State<DashboardView> {
 
   @override
   Widget build(BuildContext context) {
+    final SettingsProvider _settingsProvider  = Provider.of<SettingsProvider>(context);
+    final NotificationProvider _notificationProvider = Provider.of<NotificationProvider>(context);
+
+    final bool _isStartProcess = _settingsProvider.getBool("isStartProcess");
+
+    if(_isStartProcess) {
+        Future.delayed(
+          Duration(seconds: _settingsProvider.getInt("timer")),
+          () => _notificationProvider.createNotification(
+            title: "Petamentor has successfully completed the process.",
+            content: "Your 3D filament is ready.",
+          )
+        );
+      } 
+      if (!_isStartProcess) {
+        _notificationCompleter.complete();
+      }
+
+
+
     return Scaffold(
       body: PersistentTabView(
         context, 
