f1436d2f392a49cdc7d7b41cd8e373d4a4053251
shielamae02 <shiela.mlepon@gmail.com>
2024-05-14 00:06:02 +0800

    Update endpoints in api_constants.dart

--- lib/services/api_constants.dart
+++ lib/services/api_constants.dart
@@ -3,12 +3,11 @@ String MACHINE_CODE = "";
 
 const String BASE_URL = "https://maker-sync-django.vercel.app/api/v2/machines";
 
-String USER_URL = "$BASE_URL/$MACHINE_CODE/users";
-String SENSOR_URL = "$BASE_URL/$MACHINE_CODE/sensors";
+String USER_URL = "$BASE_URL/$MACHINE_CODE/users/";
+String SENSOR_URL = "$BASE_URL/$MACHINE_CODE/sensors/";
 String NOTIFICATION_URL = "$BASE_URL/$MACHINE_CODE/notifications";
 
 
 void updateMachineCode(String code) {
   MACHINE_CODE = code;
 }
-
