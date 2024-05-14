3051b15a9b4a726c5eafcbe96faafd1c427d74e8
shielamae02 <shiela.mlepon@gmail.com>
2024-05-14 21:28:51 +0800

    Update createNotification method in NotificationService

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -7,42 +7,48 @@ import 'package:frontend/services/api_constants.dart';
 
 class NotificationService {
   
-  Future<NotificationModel> createNotification({
-    required String title,
-    required String content
-  }) async {
+    Future<NotificationModel> createNotification({
+      required String title,
+      required String content
+    }) async {
 
-    final request = NotificationModel(
-      title: title,
-      content: content
-    );
+      final request = NotificationModel(
+        title: title,
+        content: content
+      );
 
-    final response = await http.post(
-      Uri.parse(NOTIFICATION_URL),
-      body: jsonEncode(request.toJson()),
-      headers: <String, String> {
-        "Content-Type": "application/json; charset=UTF-8"
+
+      final response = await http.post(
+        Uri.parse(NOTIFICATION_URL),
+        body: jsonEncode(request.toJson()),
+        headers: <String, String> {
+          "Content-Type": "application/json; charset=UTF-8"
+        }
+      );
+
+      if (response.statusCode == 201) {
+        return NotificationModel.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+      } else if (response.statusCode == 400) {
+        throw Exception("Notification with the same ID already exists.");
+      } else if (response.statusCode == 500) {
+        throw Exception("Internal Server Error.");
+      } else {
+        print("FAILED! : ${response.statusCode}");
+        throw Exception("Failed to create notification.");
       }
-    );
-
-    if (response.statusCode == 201) {
-      return NotificationModel.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
-    } else if (response.statusCode == 400) {
-      throw Exception("User already exists.");
-    } else if (response.statusCode == 500) {
-      throw Exception("Internal Server Error.");
-    } else {
-      throw Exception("Failed to create notification.");
     }
-  }
 
 
   Future<List<NotificationModel>> fetchNotifications() async {
     final response = await http.get(Uri.parse(NOTIFICATION_URL));
 
+    print(response.statusCode);
+
+
     if (response.statusCode == 200) {
       final List<dynamic> notifications = json.decode(response.body);
-    
+
+
       // overheat and disconnected filament
       List<NotificationModel> machineNotifications = notifications
         .map((notification) => NotificationModel.fromJson(notification as Map<String, dynamic>))
@@ -102,13 +108,15 @@ class NotificationService {
       }
 
 
-      return notifications.map((user) => NotificationModel.fromJson(user as Map<String, dynamic>)).toList();
+      return notifications.map((notification) => NotificationModel.fromJson(notification as Map<String, dynamic>)).toList();
+
     } else if (response.statusCode == 404) {
-      throw Exception("Users not found.");
+      throw Exception("Notifications not found.");
     } else if (response.statusCode == 500) {
       throw Exception("Internal Server Error.");
     } else {
-      throw Exception("Failed to fetch users.");
+      print("FAILED!: ${response.statusCode}");
+      throw Exception("Failed to fetch notifications.");
     }
   }
 
@@ -121,11 +129,11 @@ class NotificationService {
     if (response.statusCode == 200) {
       return NotificationModel.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
     } else if (response.statusCode == 404) {
-      throw Exception("Users not found.");
+      throw Exception("Notifications not found.");
     } else if (response.statusCode == 500) {
       throw Exception("Internal Server Error.");
     } else {
-      throw Exception("Failed to fetch users.");
+      throw Exception("Failed to fetch notification.");
     }
   }
 
