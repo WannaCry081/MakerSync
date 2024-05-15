13f1137d634554ab93b6946684a6a30a01339421
shielamae02 <shiela.mlepon@gmail.com>
2024-05-15 16:43:33 +0800

    Update ConnectedView

--- lib/views/Dashboard/Home/overview_views/connected_view.dart
+++ lib/views/Dashboard/Home/overview_views/connected_view.dart
@@ -1,95 +1,79 @@
-import "package:flutter/material.dart";
-import "package:flutter_screenutil/flutter_screenutil.dart";
-import "package:flutter_svg/svg.dart";
-import "package:frontend/providers/sensor_provider.dart";
-import "package:frontend/providers/settings_provider.dart";
-import "package:frontend/widgets/text_widget.dart";
-import "package:percent_indicator/circular_percent_indicator.dart";
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:frontend/providers/sensor_provider.dart';
+import 'package:frontend/providers/settings_provider.dart';
+import 'package:frontend/widgets/text_widget.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:provider/provider.dart';
 
-class ConnectedView extends StatefulWidget {
+class ConnectedView extends StatelessWidget {
   final SettingsProvider settingsProvider;
-  final SensorProvider sensorProvider;
 
   const ConnectedView({
     Key? key,
     required this.settingsProvider,
-    required this.sensorProvider,
   }) : super(key: key);
 
   @override
-  State<ConnectedView> createState() => _ConnectedViewState();
-}
-
-class _ConnectedViewState extends State<ConnectedView> {
-
-  
-
-  @override
   Widget build(BuildContext context) {
+    return Consumer<SensorProvider>(
+      builder: (context, sensorProvider, child) {
+        final sensor = sensorProvider.getSensorData();
 
-    final sensor = widget.sensorProvider.getSensorData();
+        if (sensor == null) {
+          return const CircularProgressIndicator();
+        }
 
-    if (sensor == null) {
-      return const CircularProgressIndicator(); 
-    }
-    
-    return  Padding(
-      padding: EdgeInsets.symmetric(
-        vertical: 20.h,
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          
-          SizedBox(height: 15.h),
-
-          CircularPercentIndicator(
-            radius: 120.r,
-            lineWidth: 35,
-            percent: 0,
-            // percent: sensor.timer.toDouble(),
-            progressColor: Theme.of(context).colorScheme.primary,
-            backgroundColor: Theme.of(context).colorScheme.tertiary,
-            circularStrokeCap: CircularStrokeCap.round,
-            center: MSTextWidget(
-                "0%",
-                // "${(_sensor.timer * 100).toInt()}%",
-                fontColor : Theme.of(context).colorScheme.onBackground,
-                fontSize : 45.sp,
-                fontWeight: FontWeight.w500,
-              ),
+        return Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: 20.h,
           ),
-      
-          SizedBox(height: 20.h),
-      
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.tertiary,
-                borderRadius: BorderRadius.circular(10.r)
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 15.h),
+              CircularPercentIndicator(
+                radius: 120.r,
+                lineWidth: 35,
+                percent: 0,
+                // percent: sensor.timer.toDouble() / 100,
+                progressColor: Theme.of(context).colorScheme.primary,
+                backgroundColor: Theme.of(context).colorScheme.tertiary,
+                circularStrokeCap: CircularStrokeCap.round,
+                center: MSTextWidget(
+                  "${(sensor.timer * 100).toInt()}%",
+                  fontColor: Theme.of(context).colorScheme.onBackground,
+                  fontSize: 45.sp,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SvgPicture.asset(
-                    "assets/svgs/Thermometer.svg"
+              SizedBox(height: 20.h),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.tertiary,
+                    borderRadius: BorderRadius.circular(10.r),
                   ),
-
-                  SizedBox(width: 10.w),
-                  
-                  MSTextWidget(
-                    // "0°C",
-                    "${sensor.temperature}°C",
-                    fontColor: Theme.of(context).colorScheme.onBackground,
-                    fontSize: 50.sp,
-                    fontWeight: FontWeight.bold,
-                  )
-                ]
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset("assets/svgs/Thermometer.svg"),
+                      SizedBox(width: 10.w),
+                      MSTextWidget(
+                        "${sensor.temperature}°C",
+                        fontColor: Theme.of(context).colorScheme.onBackground,
+                        fontSize: 50.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ],
+                  ),
+                ),
               ),
-            ),
-          )
-        ],
-      ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
