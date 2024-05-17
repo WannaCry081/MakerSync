8e0fa2b5218926af2711df2c585e813a72f55fa8
shielamae02 <shiela.mlepon@gmail.com>
2024-05-17 13:30:55 +0800

    Update base_url endpoint in api_constants.dart

--- lib/services/api_constants.dart
+++ lib/services/api_constants.dart
@@ -1,6 +1,6 @@
 import 'package:frontend/main.dart';
 
-const String BASE_URL = "https://maker-sync-django.vercel.app/api/v2/machines";
+const String BASE_URL = "https://maker-sync.vercel.app/api/v2/machines";
 
 String USER_URL = "$BASE_URL/$MACHINE_CODE/users";
 String SENSOR_URL = "$BASE_URL/$MACHINE_CODE/sensors";
